34948c9e2baa01d2a1eb7ae2de2af1fcedece25b
Lucas Boti <luke.boti.lorken@gmail.com>
2022-07-13 17:36:34 -0300

    styles.css created

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,19 +4,20 @@ import React from "react"
 
 function Card(props) {
     return (
-       <div className="card">
-            <img src={props.img} alt=""></img>
-            <div className="card-body">
-                <div className="form-entry">
-                    <h2 className="card-title">{props.title}</h2>
-                    <p className="card-content">{props.content}</p>
-                    <h3 className="card-price">{props.price}</h3>
-                    <button className="card-btn"></button>
-                  
-                </div>
-            </div> 
-
-       </div>
+        <div className="wrapper">
+            <div className="card">
+                    <img src={props.img} alt=""></img>
+                    <div className="card-body">
+                        <div className="form-entry">
+                            <h2 className="card-title">{props.title}</h2>
+                            <p className="card-content">{props.content}</p>
+                            <h3 className="card-price">{props.price}</h3>
+                            <button className="card-btn">Enviar</button>
+                        
+                        </div>
+                    </div> 
+            </div>
+        </div>
     )
 }
 
